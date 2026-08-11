daf8212212ed435a40f88c15f59897759adf57f8
Iaroslav Kolobkov <yar199199@gmail.com>
2026-08-11 17:42:29 +0200

    test: honest auth scenarios, stricter token assertions, negative login

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,7 +1,7 @@
 import allure
 import pytest
 
-from utils.data_generator import DEFAULT_PASSWORD, unique_username
+from utils.data_generator import unique_username, DEFAULT_PASSWORD
 from utils.encoders import decode_token, encode_password
 
 pytestmark = allure.feature("Авторизация")
@@ -9,17 +9,22 @@ pytestmark = allure.feature("Авторизация")
 
 @pytest.mark.smoke
 @pytest.mark.auth
-@allure.title("Логин возвращает токен")
-def test_login_returns_token(authenticated_user):
-    assert authenticated_user["token"]
+@allure.title("Регистрация и вход возвращают рабочий токен")
+def test_signup_and_login(auth):
+    username = unique_username()
+    auth.signup(username, DEFAULT_PASSWORD)
+    token = auth.login(username, DEFAULT_PASSWORD)
+    assert username in decode_token(token)
 
 
 @pytest.mark.auth
 @pytest.mark.regression
 @allure.title("Токен обратимо декодируется и содержит логин пользователя")
-def test_token_contains_username(authenticated_user):
+def test_token_is_reversible(authenticated_user):
     decoded = decode_token(authenticated_user["token"])
-    assert authenticated_user["username"] in decoded
+    username = authenticated_user["username"]
+    assert decoded.startswith(username)
+    assert decoded[len(username):].isdigit()
 
 
 @pytest.mark.auth
@@ -28,8 +33,16 @@ def test_token_contains_username(authenticated_user):
 def test_login_with_base64_encoded_password(auth):
     username = unique_username()
     password = encode_password(DEFAULT_PASSWORD)
-
     auth.signup(username, password)
     token = auth.login(username, password)
+    assert username in decode_token(token)
 
-    assert token
+
+@pytest.mark.auth
+@pytest.mark.negative
+@allure.title("Вход с неверным паролем отклоняется")
+def test_login_with_wrong_password(auth):
+    username = unique_username()
+    auth.signup(username, DEFAULT_PASSWORD)
+    with pytest.raises(AssertionError):
+        auth.login(username, "wrong_password")
